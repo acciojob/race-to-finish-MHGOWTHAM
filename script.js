e2b5a04066e7697442d89b6485198b7d7ba80265
GOWTHAM M H <117714418+MHGOWTHAM@users.noreply.github.com>
2023-06-27 21:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,34 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let a=new Promise((resolve)=>{
+	setTimeOut(()=>{
+		resolve();
+	},1000);
+})
+let e=new Promise((resolve)=>{
+	setTimeOut(()=>{
+		resolve();
+	},2000);
+})
+let b=new Promise((resolve)=>{
+	setTimeOut(()=>{
+		resolve();
+	},3000);
+})
+let c=new Promise((resolve)=>{
+	setTimeOut(()=>{
+		resolve();
+	},4000);
+})
+let d=new Promise((resolve)=>{
+	setTimeOut(()=>{
+		resolve();
+	},5000);
+})
+let x=Promise.any([a,b,c,d,e]);
+x.then((data)=>{
+	let p=document.getElementById("output");
+	p.innerText=data;
+})
+
